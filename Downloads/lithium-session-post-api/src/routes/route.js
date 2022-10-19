ce01ab48e089af2b0aa7380bb2c1b2989afc5a53
palsubodh <palsubodh390@gmail.com>
2022-10-19 23:01:47 +0530

    Assignment post -api

---
+++ Downloads/lithium-session-post-api/src/routes/route.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+router.get("/random" , function(req, res) {
+    res.send("hi there")
+})
+
+
+router.get("/test-api" , function(req, res) {
+    res.send("hi FunctionUp")
+})
+
+
+router.get("/test-api-2" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
+
+
+router.get("/test-api-3" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+
+
+router.get("/test-api-5" , function(req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+router.get("/test-api-6" , function(req, res) {
+    res.send({a:56, b: 45})
+})
+
+router.post("/test-post", function(req, res) {
+    res.send([ 23, 45 , 6])
+})
+
+
+router.post("/test-post-2", function(req, res) {
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+router.post("/test-post-3", function(req, res) {
+    // let id = req.body.user
+    // let pwd= req.body.password
+
+    // console.log( id , pwd)
+
+    console.log( req.body )
+
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+
+
+
+
+let player =[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    
+]
+
+
+
+
+router.post("/test-post-4", function(req, res) {
+    let player2 = req.body
+    for(let i=0;i<player2.length;i++)
+    {
+        let count=0
+        for(let j=0;j<player.length;j++)
+        {
+            if(player2[i].name==player[j].name)
+            {
+                count++
+            }
+        }
+        if(count==0)
+        {
+            player.push(player2[i])
+        }
+    
+        
+    }
+    res.send(  { data: player , status: true }  )
+})
+
+module.exports = router;
