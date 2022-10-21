bb5f4a8bfb1088ad644c970e6e5b151f87a4b3d7
palsubodh <palsubodh390@gmail.com>
2022-10-21 18:40:57 +0530

    Done mongo and connected

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
-, {
+mongoose.connect("mongodb+srv://palsubodh:Palsubodh@cluster0.mhegah9.mongodb.net/test"
+,{
    useNewUrlParser: true 
 }
-).then( () => {console.log( "MongoDb is connected")}  )
+).then( () => {console.log( "Subodh MongoDb is connected")}  )
 .catch( err => console.log(err))
 
 
