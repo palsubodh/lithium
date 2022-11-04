4e4fa580e4e05c09118cb43d265116db4b979381
palsubodh <palsubodh390@gmail.com>
2022-11-04 14:15:59 +0530

    assignment/auth-1

---
+++ src/controllers/loginController.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+const login = async function (req,res){
+
+    const {emailId, password} = req.body
+    const fetchdata = await userModel.findOne({emailId:emailId , password:password})
+    if(fetchdata==null) res.send("doesn't match the emailId and password with userdata")
+    
+    console.log("login Successfully")
+    const myToken = jwt.sign({email:emailId},'passwordSignature')
+    console.log(myToken)
+    res.send({staus:true,data:myToken})
+}
+
+module.exports.login = login
